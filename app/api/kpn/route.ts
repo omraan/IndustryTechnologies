e49024167a5ac121d9421da04a83418091bbd836
Omraan Timmerarends <omraan93@gmail.com>
2023-10-11 19:50:45 +0200

    Test

--- app/api/kpn/route.ts
+++ app/api/kpn/route.ts
@@ -1,53 +1,21 @@
 import { db } from "@/firebase";
-import { child, get, ref, set, update } from "firebase/database";
+import { child, get, getDatabase, onValue, ref, set, update } from "firebase/database";
 import { NextRequest, NextResponse } from "next/server";
 
-interface PostBody {
-	[key: string]: {
-		[key: string]: {
-			time: number;
-			value: string;
-			unit: string;
-		};
-	};
-}
-
 export async function GET(request: Request) {
-	return NextResponse.json({ text: "test" }, { status: 200 });
-}
+	const kpnRef = ref(db, "kpn");
 
-export async function POST(req: NextRequest) {
-	const body = await req.json();
-	const device = body[0].bn;
-	const time = body[0].bt;
-	let postBody: PostBody = {};
-	let d;
-	body.map(async (x: any, index: number) => {
-		if (index > 0) {
-			const kpnRef = child(ref(db), `kpn/${device}/${x.n}`);
-
-			const snapshot = await get(kpnRef);
-			d = snapshot;
-			const data = snapshot.val();
-
-			postBody[x.n] = {
-				...data,
-				[time]: {
-					time: time,
-					value: x.v ? x.v : parseFloat(x.vs).toFixed(1),
-					unit: x.u,
-				},
-			};
-			await update(kpnRef, postBody[x.n]);
-		}
-	});
-
-	// const kpnRef = child(ref(db), `kpn/${device}/${body[4].n}`);
-	// const snapshot = await get(kpnRef);
-	return new Response(JSON.stringify({ d: postBody }), {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
+	const data = await onValue(
+		kpnRef,
+		(snapshot) => {
+			// Get the data from the snapshot
+			const data: Record<string, any> = snapshot.val();
+			return data[Object.keys(data)[0]];
 		},
-	});
+		(error) => {
+			console.log("Error retrieving data:", error);
+		}
+	);
+	console.log(data);
+	return NextResponse.json({ data: data }, { status: 200 });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "@/components/Header";
 export default function Home() {
+	const data = [
+		{ bn: "urn:dev:DVNUUID:1513bfb2-b495-494d-b8d8-9dead23190a3:", bt: 1695586444 },
+		{ n: "battery", u: "%", vs: "33.0" },
+		{ n: "accelerationX", u: "m/s2", v: -0.13 },
+		{ n: "accelerationY", u: "m/s2", v: 1.42 },
+		{ n: "accelerationZ", u: "m/s2", v: 9.7 },
+	];
+	console.log(data.slice(1));
 	return (
 		<main className="">
 			<Header />
